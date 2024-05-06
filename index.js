c6796f43257ebd3d8acc23f01f1124cd970e2420
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-05-06 19:39:04 +0500

    Currency Converted completed

---
+++ index.js
@@ -0,0 +1,33 @@
+import inquirer from 'inquirer';
+let currency = {
+    USD: 1, //base currency
+    EUR: 0.93,
+    GBP: 0.79,
+    INR: 83.49,
+    PKR: 278.24
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Convert Currency From?",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "to",
+        message: "Convert Currency To?",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "Enter Amount",
+        type: "number",
+    }
+]);
+let fromcurrency = currency[user_answer.from];
+let tocurrency = currency[user_answer.to];
+let useramount = user_answer.amount;
+let baseconvert = useramount / fromcurrency;
+let finalresult = baseconvert * tocurrency;
+console.log(`Your Currency Conversion is ` + Math.round(finalresult));
